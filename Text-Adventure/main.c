179c3d1ded5cfdfce19c559a60d2d18ca9c8d56c
ttury <lovekj96@naver.com>
2020-10-10 19:08:01 +0900

    add print Ascii-Art function

--- Text-Adventure/main.c
+++ Text-Adventure/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+void printAsciiArt(char *nameOfAsciiArt) {
+	char print_temp[256];
+	FILE* rfp;
+	char pathOfAsciiArt[100];
+	sprintf(pathOfAsciiArt, ".\\Ascii-Art\\%s", nameOfAsciiArt);
+	rfp = fopen(pathOfAsciiArt, "r");
+
+	system("mode con cols=130");
+
+	if (rfp == NULL) {
+		printf("%s 파일을 불러올 수 없습니다.\n", nameOfAsciiArt);
+		return;
+	}
+
+	while (fgets(print_temp, 255, rfp) != NULL) {
+		printf(print_temp);
+	}
+	puts("");
+
+	fclose(rfp);
+
+	return;
+}
 
 int main() {
-	printf("hello world");
+	printAsciiArt("thinkingface.txt");
 	return 0;
 }
